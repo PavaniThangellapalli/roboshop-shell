48a281f9044349de140c6574cc6f112c31bf6e5b
pavani <pavani.th93@gmail.com>
2023-03-21 16:06:28 +0000

    Keeping code DRY

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head "setup mongodb repository"
 cp Configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install Mongodb"
 yum install mongodb-org -y 
+
+print_head "Enable Mongodb"
 systemctl enable mongod 
+
+print_head "Start Mongodb"
 systemctl start mongod 
 
 # Update file /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
